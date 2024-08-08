518911db21a0e55e768b31948ea442de97b4e823
Life Of Coder <82974657+omer-faruk-ozmen@users.noreply.github.com>
2024-08-08 15:37:33 +0300

    database env name changed

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=3) 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_TOKEN_LOCATION = ['cookies']
